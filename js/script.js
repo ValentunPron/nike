0c706dc66e6ef07dca888a823ebe6ba0fedf65bc
Valentyn <valentunpron@gmail.com>
2023-03-20 20:00:36 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const header = document.querySelector('.header'),
+	  headerBurger = document.querySelector('.header__burger');
+
+headerBurger.addEventListener('click', (event) => {
+	event.preventDefault();
+	header.classList.toggle('active');
+})
