f20e4b847f8e6cde3eed9f716b4d837b013a275d
Valentyn <valentunpron@gmail.com>
2023-03-23 00:12:28 +0200

    Анімація блокуЄ

--- js/moduls/scrollAnimation.js
+++ js/moduls/scrollAnimation.js
@@ -1,18 +1,53 @@
 const scrollAnimation = () => {
-	gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+	gsap.registerPlugin(ScrollTrigger, ScrollSmoother, TimelineMax);
 
-	if (ScrollTrigger.isTouch !== 1) {	
-		gsap.fromTo('.hero__image', { opacity: 0, x: 300}, {opacity: 1, x: 0});
-		gsap.fromTo('.hero__bottom', { opacity: 0, y: 100},  {
-			opacity: 1, 
-			y: 0,
-			scrollTrigger: {
-				trigger: '.hero__content',
-				start: '-200',
-				end: '-50',
-				scrub: true
-			}
-		});
+	if (window.innerWidth <= 1400) {
+		if (ScrollTrigger.isTouch !== 1) {	
+			gsap.fromTo('.hero__image', { opacity: 0, x: 300}, {opacity: 1, x: 0});
+
+			gsap.fromTo('.hero__bottom', { opacity: 0, y: 100},  {
+				opacity: 1, 
+				y: 0,
+				scrollTrigger: {
+					trigger: '.hero__content',
+					start: '-200',
+					end: '-50',
+					scrub: true
+				}
+			});
+
+			gsap.fromTo('.view__text-block', { opacity: 0, x: -200},  {
+				opacity: 1, 
+				x: 0,
+				scrollTrigger: {
+					trigger: '.view__body',
+					start: '-300',
+					end: '-50',
+					scrub: true
+				}
+			});
+			gsap.fromTo('.view__video', { opacity: 0, x: 200},  {
+				opacity: 1, 
+				x: 0,
+				scrollTrigger: {
+					trigger: '.view__body',
+					start: '-300',
+					end: '-50',
+					scrub: true
+				}
+			});
+
+			gsap.fromTo('.hero__bottom', { opacity: 0, y: 100},  {
+				opacity: 1, 
+				y: 0,
+				scrollTrigger: {
+					trigger: '.hero__content',
+					start: '-200',
+					end: '-50',
+					scrub: true
+				}
+			});
+		}
 	}
 }
 
